3f479c7e2a0725924096b29213cae741b876deb7
josiaYvan <josiayvan@gmail.com>
2022-11-16 10:03:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  // Link
-} from "react-router-dom";
+import "./styles/App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from './pages/Home'
-import QRgen from './pages/QRgenerator'
-import QRscan from './pages/QRscanner'
+import Home from "./pages/Home";
+import QRgen from "./pages/QRgenerator";
+import QRscan from "./pages/QRscanner";
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-      
         <Router>
           <div>
-
             <Switch>
               <Route exact path="/">
-                <Home/>
+                <Home />
               </Route>
               <Route path="/qr_generator">
-                <QRgen/>
+                <QRgen />
               </Route>
               <Route path="/qr_scanner">
-                <QRscan/>
+                <QRscan />
               </Route>
             </Switch>
-
           </div>
         </Router>
-
       </div>
     </div>
   );
